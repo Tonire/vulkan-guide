b0addfc87ce1ad5d2b82524f4d645a7230bdf9e4
Toni Rebollo <tonire.rebollo@gmail.com>
2025-01-10 15:21:54 +0100

    Guide progress

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -4,16 +4,156 @@
 #pragma once
 
 #include <vk_types.h>
+#include <vk_descriptors.h>
+
+#include <vk_loader.h>
+
+struct DeletionQueue
+{
+	std::deque<std::function<void()>> deletors;
+
+	void push_function(std::function<void()> && function)
+	{
+		deletors.push_back(function);
+	}
+
+	void flush()
+	{
+		for (auto it = deletors.rbegin(); it != deletors.rend(); it++)
+		{
+			(*it)();
+		}
+		deletors.clear();
+	}
+};
+
+struct FrameData
+{
+	VkCommandPool _commandPool;
+	VkCommandBuffer _mainCommandBuffer;
+	VkSemaphore _swapchainSemaphore;
+	VkSemaphore _renderSemaphore;
+	VkFence _renderFence;
+
+	DeletionQueue _deletionQueue;
+	DescriptorAllocatorGrowable _frameDescriptors;
+};
+
+struct ComputePushConstants
+{
+	glm::vec4 data1;
+	glm::vec4 data2;
+	glm::vec4 data3;
+	glm::vec4 data4;
+};
+
+struct ComputeEffect
+{
+	const char* name;
+
+	VkPipeline pipeline;
+	VkPipelineLayout layout;
+
+	ComputePushConstants data;
+};
+
+struct GPUSceneData
+{
+	glm::mat4 view;
+	glm::mat4 proj;
+	glm::mat4 viewproj;
+	glm::vec4 ambientColor;
+	glm::vec4 sunlightDirection;
+	glm::vec4 sunlightColor;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 2;
 
 class VulkanEngine {
 public:
 
+	VulkanEngine()
+		: _instance(VK_NULL_HANDLE), _device(VK_NULL_HANDLE), _surface(VK_NULL_HANDLE), _swapchain(VK_NULL_HANDLE) {
+	}
+
 	bool _isInitialized{ false };
 	int _frameNumber {0};
-
+	bool stop_rendering{ false };
+	bool resize_requested{ false };
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
+	FrameData _frames[FRAME_OVERLAP];
+	FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+
 	struct SDL_Window* _window{ nullptr };
+
+	static VulkanEngine& Get();
+
+	VkQueue _graphicsQueue;
+	uint32_t _graphicsQueueFamily;
+
+	VkInstance _instance;
+	VkDebugUtilsMessengerEXT _debug_messenger;
+	VkPhysicalDevice _chosenGPU;
+	VkDevice _device;
+	VkSurfaceKHR _surface;
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+
+	DeletionQueue _mainDeletionQueue;
+
+	VmaAllocator _allocator;
+
+	// draw resources
+	AllocatedImage _drawImage;
+	AllocatedImage _depthImage;
+	VkExtent2D _drawExtent;
+	float renderScale = 1.0f;
+
+	DescriptorAllocator globalDescriptorAllocator;
+	VkDescriptorSet _drawImageDescriptors;
+	VkDescriptorSetLayout _drawImageDescriptorLayout;
+	VkDescriptorSetLayout _singleImageDescriptorLayout;
+
+	GPUSceneData sceneData;
+
+	VkDescriptorSetLayout _gpuSceneDataDescriptorLayout;
+
+	VkPipeline _gradientPipeline;
+	VkPipelineLayout _gradientPipelineLayout;
+
+	AllocatedImage _whiteImage;
+	AllocatedImage _blackImage;
+	AllocatedImage _greyImage;
+	AllocatedImage _errorCheckerboardImage;
+
+	VkSampler _defaultSamplerLinear;
+	VkSampler _defaultSamplerNearest;
+
+	// immediate submit structures
+	VkFence _immFence;
+	VkCommandBuffer _immCommandBuffer;
+	VkCommandPool _immCommandPool;
+
+	std::vector<ComputeEffect> backgroundEffects;
+	int currentBackgroundEffect{0};
+	
+	VkPipelineLayout _trianglePipelineLayout;
+	VkPipeline _trianglePipeline;
+
+	VkPipelineLayout _meshPipelineLayout;
+	VkPipeline _meshPipeline;
+
+	//GPUMeshBuffers rectangle;
+
+	std::vector<std::shared_ptr<MeshAsset>> testMeshes;
+
+	int captureResult = 0;
 
 	//initializes everything in the engine
 	void init();
@@ -24,6 +164,43 @@ public:
 	//draw loop
 	void draw();
 
+	void draw_background(VkCommandBuffer cmd);
+
+	void draw_geometry(VkCommandBuffer cmd);
+
+	void draw_imgui(VkCommandBuffer cmd, VkImageView targetImageView);
+
 	//run main loop
 	void run();
+
+	void immediate_submit(std::function<void(VkCommandBuffer cmd)>&& function);
+
+	AllocatedBuffer create_buffer(size_t allocSize, VkBufferUsageFlags usage, VmaMemoryUsage memoryUsage);
+	void destroy_buffer(const AllocatedBuffer& buffer);
+
+	AllocatedImage create_image(VkExtent3D size, VkFormat format, VkImageUsageFlags usage, bool bMipmapped = false);
+	AllocatedImage create_image(void* data, VkExtent3D size, VkFormat format, VkImageUsageFlags usage, bool bMipmapped = false);
+	void destroy_image(const AllocatedImage& image);
+
+	GPUMeshBuffers upload_mesh(std::span<uint32_t> indices, std::span<Vertex> vertices);
+
+private:
+
+	void init_vulkan();
+	void create_swapchain(uint32_t width, uint32_t height);
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+	void init_descriptors();
+	void init_pipelines();
+	void init_background_pipelines();
+	void init_triangle_pipeline();
+	void init_mesh_pipeline();
+	void init_imgui();
+	void init_default_data();
+
+	void resize_swapchain();
+
+	void destroy_swapchain();
+
 };
---
+++ src/vk_loader.cpp
@@ -0,0 +1,140 @@
+﻿
+#include <vk_loader.h>
+
+#include "stb_image.h"
+#include <iostream>
+
+#include "vk_engine.h"
+#include "vk_initializers.h"
+#include "vk_types.h"
+#include <glm/gtx/quaternion.hpp>
+
+#include <fastgltf/glm_element_traits.hpp>
+#include <fastgltf/parser.hpp>
+#include <fastgltf/tools.hpp>
+#include <fmt/core.h>
+
+std::optional<std::vector<std::shared_ptr<MeshAsset>>> loadGltfMeshes(VulkanEngine* engine, std::filesystem::path filePath)
+{
+	fmt::print("Loading GLTF: {} \n", filePath.string());
+	fastgltf::GltfDataBuffer data;
+	data.loadFromFile(filePath);
+
+	constexpr auto gltfOptions = fastgltf::Options::LoadGLBBuffers | fastgltf::Options::LoadExternalBuffers;
+
+	fastgltf::Asset gltf;
+	fastgltf::Parser parser{};
+
+	auto load = parser.loadBinaryGLTF(&data, filePath.parent_path(), gltfOptions);
+	if (load)
+	{
+		gltf = std::move(load.get());
+	}
+	else
+	{
+		fmt::println("Failed to load glTF: {}", fastgltf::to_underlying(load.error()));
+		return{};
+	}
+	
+	std::vector<std::shared_ptr<MeshAsset>> meshes;
+
+	std::vector<uint32_t> indices;
+	std::vector<Vertex> vertices;
+	for (fastgltf::Mesh& mesh : gltf.meshes)
+	{
+		MeshAsset newMesh;
+
+		newMesh.name = mesh.name;
+
+		indices.clear();
+		vertices.clear();
+
+		for (auto&& p : mesh.primitives)
+		{
+			GeoSurface newSurface;
+			newSurface.startIndex = (uint32_t)indices.size();
+			newSurface.count = (uint32_t)gltf.accessors[p.indicesAccessor.value()].count;
+
+			size_t initial_vtx = vertices.size();
+
+			// load indexes
+			{
+				fastgltf::Accessor& indexAccessor = gltf.accessors[p.indicesAccessor.value()];
+				indices.reserve(indices.size() + indexAccessor.count);
+				fastgltf::iterateAccessor<std::uint32_t>(gltf, indexAccessor, 
+					[&](std::uint32_t idx) {
+						indices.push_back(idx + (uint32_t)initial_vtx);
+					});
+			}
+
+			// load vertex positions
+			{
+				fastgltf::Accessor& posAccessor = gltf.accessors[p.findAttribute("POSITION")->second];
+				vertices.resize(vertices.size() + posAccessor.count);
+				fastgltf::iterateAccessorWithIndex<glm::vec3>(gltf, posAccessor,
+					[&](glm::vec3 v, size_t index)
+					{
+						Vertex newVert;
+						newVert.position = v;
+						newVert.normal = { 1, 0, 0 };
+						newVert.color = glm::vec4{ 1.0f };
+						newVert.uv_x = 0.0f;
+						newVert.uv_y = 0.0f;
+						vertices[initial_vtx + index] = newVert;
+					});
+			}
+
+			// load vertex normals
+			{
+				auto normals = p.findAttribute("NORMAL");
+				if (normals != p.attributes.end())
+				{
+					fastgltf::iterateAccessorWithIndex<glm::vec3>(gltf, gltf.accessors[(*normals).second],
+						[&](glm::vec3 v, size_t index)
+						{
+							vertices[initial_vtx + index].normal = v;
+						});
+				}
+			}
+
+			// load UVs
+			auto uv = p.findAttribute("TEXCOORD_0");
+			if (uv != p.attributes.end())
+			{
+				fastgltf::iterateAccessorWithIndex<glm::vec2>(gltf, gltf.accessors[(*uv).second],
+					[&](glm::vec2 v, size_t index)
+					{
+						vertices[initial_vtx + index].uv_x = v.x;
+						vertices[initial_vtx + index].uv_y = v.y;
+					});
+			}
+
+			// load vertex colors
+			auto colors = p.findAttribute("COLOR_0");
+			if (colors != p.attributes.end())
+			{
+				fastgltf::iterateAccessorWithIndex<glm::vec4>(gltf, gltf.accessors[(*colors).second],
+				[&](glm::vec4 v, size_t index)
+				{
+					vertices[initial_vtx + index].color = v;
+				});
+			}
+
+			newMesh.surfaces.push_back(newSurface);
+
+			// display the vertex normals
+			constexpr bool bOverrideColors = false;
+			if (bOverrideColors)
+			{
+				for (Vertex& v : vertices)
+				{
+					v.color = glm::vec4(v.normal, 1.0f);
+				}
+			}
+			
+			newMesh.meshBuffers = engine->upload_mesh(indices, vertices);
+            meshes.emplace_back(std::make_shared<MeshAsset>(newMesh));
+		}
+	}
+	return meshes;
+}
